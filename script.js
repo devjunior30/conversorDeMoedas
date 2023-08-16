8952acbc5827b6f48b5a902cd2851fb23a19d3dc
devjunior30 <edyjunior299@gmail.com>
2023-08-15 22:07:24 -0300

    iniciando no java script e alterações no html

---
+++ script.js
@@ -0,0 +1,11 @@
+function soma() {
+    let real = document.getElementById("real").value
+    let dolar = document.getElementById("dolar").value
+
+    let resultado = Math.floor(real/5.08)
+
+    firstP.innerHTML = "$" + resultado
+
+    label.innerHTML = "R$" + resultados
+     
+}

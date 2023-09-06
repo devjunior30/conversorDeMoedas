cad2610a98ca7a19a77ef2cf828b500041871923
devjunior30 <edyjunior299@gmail.com>
2023-09-06 00:19:29 -0300

    exclusão de algumas imagens e novas moedas

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let selectsReplace = document.querySelector(".selects")
 function converted() {
     let inputValue = document.querySelector("#inputValues").value
     let dolarToday = 5.2
+    let libraToday = 4.2
+    let bitconToday = 3.10
     let euroToday = 5.20
 
     if (selectsReplace.value == "dolar") {
@@ -33,6 +35,32 @@ function converted() {
         }).format(inputValue)
     }
 
+     if (selectsReplace.value == "libra") {
+
+        US.innerHTML = new Intl.NumberFormat("ar-Ly ", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputValue / libraToday)
+
+        BRL.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputValue)
+    }
+
+     if (selectsReplace.value == "bitcon") {
+
+        US.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValue / euroToday)
+
+        BRL.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputValue)
+    }
+
 }
 
 function changeSelect() {
@@ -47,6 +75,16 @@ function changeSelect() {
     if (selectsReplace.value == "euro") {
         textCurrency.innerHTML = "Euro"
         image.src = "/assets/euro.png"
+    }
+
+    if (selectsReplace.value == "libra") {
+        textCurrency.innerHTML = "Libra"
+        image.src = "/assets/libra.png"
+    }
+
+    if (selectsReplace.value == "bitcon") {
+        textCurrency.innerHTML = "Bitcons"
+        image.src = "/assets/bitcoin.png"
     }
 
     converted()
